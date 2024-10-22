6c724d13c6a68296c751d76299d1c4075482659d
Fábio Luiz Nery de Miranda <fabio@7gen.com>
2024-10-22 10:19:04 -0700

    CssBaseline - enableColorScheme (dark mode settings)

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,11 +27,14 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <CssBaseline />
       </head>
       <body suppressHydrationWarning className={roboto.variable}>
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
+            {/* https://web.dev/articles/color-scheme */}
+            {/* https://mui.com/material-ui/react-css-baseline/#color-scheme */}
+            {/* https://mui.com/material-ui/customization/dark-mode/#:~:text=Adding%20%3CCssBaseline%20/%3E%20inside%20of%20the%20%3CThemeProvider%3E%20component%20will%20also%20enable%20dark%20mode%20for%20the%20app%27s%20background. */}
+            <CssBaseline enableColorScheme />
             {children}
           </ThemeProvider>
         </AppRouterCacheProvider>
