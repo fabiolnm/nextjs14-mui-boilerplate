e0adfc8f5b063d36748cd82e246430fc89566a00
Fábio Luiz Nery de Miranda <fabio@7gen.com>
2024-10-22 17:51:25 -0700

    Configured import/order eslint plugin

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import { CssBaseline } from '@mui/material'
-import type { Metadata } from 'next'
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
-
-import { Roboto } from 'next/font/google'
 import { ThemeProvider } from '@mui/material/styles'
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
+import type { Metadata } from 'next'
+import { Roboto } from 'next/font/google'
+
 import { theme } from './theme'
 
 const roboto = Roboto({
